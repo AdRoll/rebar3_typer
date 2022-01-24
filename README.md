524653cdf96462d3f50165d002669cc6beee5176
Brujo Benavides <fernando.benavides@adroll.com>
2022-01-24 14:46:49 +0100

    Fix #3: Parse rebar.config (#6)
    * Match Typer options in the plugin
    
    * Fix --version option definition
    
    * Do not lint for now rebar3_typer_prv
    
    * Fix #3: Parse rebar.config
    
    * Fix after testing
    
    * Add undocumented options
    
    * Update list of option definitions
    
    * Use the rebar3 way… but no macros
    
    * Fix Linting Warnings
    
    * Equalize options
    
    * Add Docs
    
    Co-authored-by: Pablo Costas Sánchez <pablo.costas@nextroll.com>

--- README.md
+++ README.md
@@ -23,3 +23,69 @@ $ rebar3 typer
 ===> Compiling rebar3_typer
 <Plugin Output>
 ```
+
+## Configuration
+
+You can basically use the same command-line options as you can use with the original TypEr:
+
+```man
+Usage: rebar3 typer [-r <recursive>] [--show [<show>]]
+                    [--show-exported [<show_exported>]]
+                    [--show_exported [<show_exported>]]
+                    [--show-success-typings [<show_success_typings>]]
+                    [--show_success_typings [<show_success_typings>]]
+                    [--annotate [<annotate>]]
+                    [--annotate-inc-files [<annotate_inc_files>]]
+                    [--no_spec [<no_spec>]] [--edoc [<edoc>]]
+                    [--plt <plt>] [-T <typespec_files>] [-v]
+
+  -r                      Search comma-separated directories recursively
+                          for .erl files below them.
+  --show                  Print type specifications for all functions on
+                          stdout. [default: true]
+  --show-exported         Same as --show, but print specifications for
+                          exported functions only.Specs are displayed
+                          sorted alphabetically on the function's name.
+                          [default: false]
+  --show_exported         Same as --show-exported. [default: false]
+  --show-success-typings  Show the success typings inferred by Dialyzer /
+                          Typer. This is an undocumented option. [default:
+                          false]
+  --show_success_typings  Same as --show-success-typings. This is an
+                          undocumented option. [default: false]
+  --annotate              Annotate the specified files with type
+                          specifications. [default: false]
+  --annotate-inc-files    Same as --annotate but annotates all -include()
+                          files as well as all .erl files. (Use this
+                          option with caution - it has not been tested
+                          much). [default: false]
+  --no_spec               Ignore the specs from the files. This is an
+                          undocumented option. [default: false]
+  --edoc                  Print type information as Edoc @spec comments,
+                          not as type specs. [default: false]
+  --plt                   Use the specified dialyzer PLT file rather than
+                          the default one.
+  -T                      The specified file(s) already contain type
+                          specifications and these are to be trusted in
+                          order to print specs for the rest of the files.
+                          (Multiple files or dirs, separated by commas,
+                          can be specified.)
+  -v, --version           Print the TypEr version and some more
+                          information and exit.
+```
+
+You can also put those options directly in your `rebar.config` file…
+
+```erlang
+{typer,
+    [{show, true},                  %% Print type specifications for all functions on stdout.
+     {show_exported, false},        %% Same as --show, but print specifications for exported functions only.
+     {annotate, false},             %% Annotate the specified files with type specifications.
+     {annotate_inc_files, false},   %% Same as --annotate but annotates all -include() files as well as all .erl files.
+     {edoc, false},                 %% Print type information as Edoc @spec comments, not as type specs.
+     {plt, "/path/to/plt"},         %% Use the specified dialyzer PLT file rather than the default one.
+     {show_success_typings, false}, %% Show the success typings inferred by Dialyzer / Typer.
+     {no_spec, false},              %% Ignore existing function specs.
+     {typespec_files, ["f1", "f2"]} %% The specified file(s) already contain type specifications.
+    ]}
+```
--- src/rebar3_typer_prv.erl
+++ src/rebar3_typer_prv.erl
@@ -10,11 +10,6 @@
               {rebar_state, command_parsed_args, 1}]).
 
 -define(PROVIDER, typer).
--define(OPTS,
-        [{boolean, $b, "boolean", {boolean, false}, "Boolean example"},
-         {string, $s, "rebar-config", {string, "some.txt"}, "String example"},
-         {atom, $a, "ignore", atom, "Atom example"},
-         {atom_none, $n, "atom-none", {atom, none}, "Atom example, with default"}]).
 
 %% =============================================================================
 %% Public API
@@ -28,23 +23,30 @@ init(State) ->
                           {bare, true},      % The task can be run by the user, always true
                           {deps, []},        % The list of dependencies
                           {example, "rebar3 typer"}, % How to use the plugin
-                          {opts, ?OPTS},     % list of options understood by the plugin
+                          {opts, opts()},    % list of options understood by the plugin
                           {short_desc, "Execute TypEr on your code"},
                           {desc, "Execute TypEr on your code"}]),
     {ok, rebar_state:add_provider(State, Provider)}.
 
--spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
+-spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, {rebar3_typer_prv, term()}}.
 do(State) ->
-    Opts = parse_opts(State),
-    ok = rebar_api:debug("Opts: ~p", [Opts]),
-    case Opts of
-        #{boolean := true} ->
-            {ok, State};
-        _ ->
-            {error, io_lib:format("Not implemented yet. Opts: ~p", [Opts])}
+    try
+        rebar_api:info("Looking for types to add...", []),
+        CmdLineOpts = parse_opts(State),
+        RebarConfigOpts = parse_rebar_config(State),
+        Opts = maps:merge(RebarConfigOpts, CmdLineOpts),
+        ok = rebar_api:debug("Opts: ~p", [Opts]),
+        {error, {?MODULE, not_implemented}}
+    catch
+        error:{unrecognized_opt, Opt} ->
+            {error, {?MODULE, {unrecognized_opt, Opt}}}
     end.
 
 -spec format_error(any()) -> iolist().
+format_error(not_implemented) ->
+    io_lib:format("Not yet implemented.", []);
+format_error({unrecognized_opt, Opt}) ->
+    io_lib:format("Unrecognized option in rebar.config: ~p", [Opt]);
 format_error(Reason) ->
     io_lib:format("~p", [Reason]).
 
@@ -52,10 +54,126 @@ format_error(Reason) ->
 %% Internal functions
 %% =============================================================================
 
--spec parse_opts(rebar_state:t()) -> maps:map().
 parse_opts(State) ->
-    {Args, _} = rebar_state:command_parsed_args(State),
-    #{boolean => proplists:get_value(boolean, Args),
-      string => proplists:get_value(string, Args),
-      atom => proplists:get_value(atom, Args),
-      atom_none => proplists:get_value(atom_none, Args)}.
+    {CliOpts, _} = rebar_state:command_parsed_args(State),
+    parse_cli_opts(CliOpts, #{}).
+
+parse_cli_opts([], Acc) ->
+    Acc;
+parse_cli_opts([{recursive, Dirs} | T], Acc) ->
+    parse_cli_opts(T, Acc#{recursive => split_string(Dirs)});
+parse_cli_opts([{show, Bool} | T], Acc) ->
+    parse_cli_opts(T, Acc#{show => Bool});
+parse_cli_opts([{show_exported, Bool} | T], Acc) ->
+    parse_cli_opts(T, Acc#{show_exported => Bool});
+parse_cli_opts([{show_success_typings, Bool} | T], Acc) ->
+    parse_cli_opts(T, Acc#{show_success_typings => Bool});
+parse_cli_opts([{annotate, Bool} | T], Acc) ->
+    parse_cli_opts(T, Acc#{annotate => Bool});
+parse_cli_opts([{annotate_inc_files, Bool} | T], Acc) ->
+    parse_cli_opts(T, Acc#{annotate_inc_files => Bool});
+parse_cli_opts([{no_spec, Bool} | T], Acc) ->
+    parse_cli_opts(T, Acc#{no_spec => Bool});
+parse_cli_opts([{edoc, Bool} | T], Acc) ->
+    parse_cli_opts(T, Acc#{edoc => Bool});
+parse_cli_opts([{plt, PltFile} | T], Acc) ->
+    parse_cli_opts(T, Acc#{plt => PltFile});
+parse_cli_opts([{typespec_files, Files} | T], Acc) ->
+    parse_cli_opts(T, Acc#{typespec_files => split_string(Files)});
+parse_cli_opts([version | T], Acc) ->
+    parse_cli_opts(T, Acc#{version => true});
+parse_cli_opts([Opt | _T], _Acc) ->
+    %% consider changing this to the rebar3 way of ?PRV_ERROR/1
+    %% TODO: catch the error
+    error({unrecognized_opt, Opt}).
+
+split_string(String) ->
+    rebar_string:lexemes(String, [$,]).
+
+%% @todo consider adding shorthand versions to some (or all) options,
+%%       even if it doesn't exist on TypEr itself
+opts() ->
+    [{recursive,
+      $r,
+      undefined,
+      string,
+      "Search comma-separated directories recursively for .erl files below them."},
+     {show,
+      undefined,
+      "show",
+      {boolean, true},
+      "Print type specifications for all functions on stdout."},
+     {show_exported,
+      undefined,
+      "show-exported",
+      {boolean, false},
+      "Same as --show, but print specifications for exported functions only."
+      "Specs are displayed sorted alphabetically on the function's name."},
+     {show_exported, undefined, "show_exported", {boolean, false}, "Same as --show-exported."},
+     {show_success_typings,
+      undefined,
+      "show-success-typings",
+      {boolean, false},
+      "Show the success typings inferred by Dialyzer / Typer. This is an undocumented option."},
+     {show_success_typings,
+      undefined,
+      "show_success_typings",
+      {boolean, false},
+      "Same as --show-success-typings. This is an undocumented option."},
+     {annotate,
+      undefined,
+      "annotate",
+      {boolean, false},
+      "Annotate the specified files with type specifications."},
+     {annotate_inc_files,
+      undefined,
+      "annotate-inc-files",
+      {boolean, false},
+      "Same as --annotate but annotates all -include() files as well as all .erl files."
+      " (Use this option with caution - it has not been tested much)."},
+     {no_spec,
+      undefined,
+      "no_spec",
+      {boolean, false},
+      "Ignore the specs from the files. This is an undocumented option."},
+     {edoc,
+      undefined,
+      "edoc",
+      {boolean, false},
+      "Print type information as Edoc @spec comments, not as type specs."},
+     {plt,
+      undefined,
+      "plt",
+      string,
+      "Use the specified dialyzer PLT file rather than the default one."},
+     {typespec_files,
+      $T,
+      undefined,
+      string,
+      "The specified file(s) already contain type specifications and these are to be trusted "
+      "in order to print specs for the rest of the files. (Multiple files or dirs, separated "
+      "by commas, can be specified.)"},
+     {version,
+      $v,
+      "version",
+      undefined,
+      "Print the TypEr version and some more information and exit."}].
+
+parse_rebar_config(State) ->
+    Config = rebar_state:get(State, typer, []),
+    lists:foldl(fun parse_rebar_config/2, #{}, proplists:unfold(Config)).
+
+parse_rebar_config({Key, Value}, Opts)
+    when Key == show;
+         Key == show_exported;
+         Key == annotate;
+         Key == annotate_inc_files;
+         Key == edoc;
+         Key == plt;
+         Key == typespec_files;
+         Key == show_success_typings;
+         Key == no_spec;
+         Key == recursive ->
+    Opts#{Key => Value};
+parse_rebar_config(Opt, _Opts) ->
+    error({unrecognized_opt, Opt}).
