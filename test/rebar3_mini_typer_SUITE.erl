6924c06c96d4a7387d7f13c9bc30330e3b417327
Brujo Benavides <elbrujohalcon@gmail.com>
2022-01-26 15:37:07 +0100

    Add more tests

--- test/rebar3_mini_typer_SUITE.erl
+++ test/rebar3_mini_typer_SUITE.erl
@@ -4,12 +4,12 @@
 -behaviour(ct_suite).
 
 -export([all/0]).
--export([no_files/1]).
+-export([empty/1, single_file/1, annotate/1]).
 
 all() ->
-    [no_files].
+    [empty, single_file, annotate].
 
-no_files(_) ->
+empty(_) ->
     ct:comment("With no files... we get an error"),
     [{abort, <<"typer: no file(s) to analyze">>}] = run_typer(#{}),
 
@@ -21,6 +21,39 @@ no_files(_) ->
     [{abort, <<"typer: no file(s) to analyze">>}] =
         run_typer(#{files_r => [abs_test_path("empty")]}),
     {comment, ""}.
+
+single_file(_) ->
+    ct:comment("With a single module... we get its types"),
+    [{info, <<"\n%% File", _/binary>>},
+     {info, <<"%% ----", _/binary>>},
+     {info, <<"-spec exported() -> 'ok'.">>},
+     {info, <<"-spec not_exported() -> 'ok'.">>}] =
+        run_typer(#{files_r => [abs_test_path("single_file")]}),
+
+    ct:comment("With a single module, and mode:show_exported... we get its exported types"),
+    [{info, <<"\n%% File", _/binary>>},
+     {info, <<"%% ----", _/binary>>},
+     {info, <<"-spec exported() -> 'ok'.">>}] =
+        run_typer(#{files_r => [abs_test_path("single_file")], mode => show_exported}),
+    {comment, ""}.
+
+%% @todo Test annotate_inc_files when https://github.com/erlang/otp/issues/5653 is fixed.
+annotate(_) ->
+    _ = file:del_dir_r(abs_test_path("annotate/typer_ann")),
+    ct:comment("With mode:annotate... only erl files are annotated"),
+    [{info, <<"      Processing file: ", _/binary>>},
+     {info, <<"             Saved as: \"", PathAndQuote1/binary>>}] =
+        run_typer(#{files_r => [abs_test_path("annotate")], mode => annotate}),
+    [$\" | ReversedPath1] = lists:reverse(binary_to_list(PathAndQuote1)),
+    {ok, Text} =
+        file:read_file(
+            lists:reverse(ReversedPath1)),
+    [<<"exported() -> {'not_exported','included'}.">>,
+     <<"not_exported() -> 'not_exported'.">>] =
+        [Spec || <<"-spec ", Spec/binary>> <- binary:split(Text, <<"\n">>, [global, trim])],
+    {comment, ""}.
+
+%%% PRIVATE FUNCTIONS
 
 run_typer(Opts) ->
     DefaultOpts = #{mode => show, io => default_io()},
